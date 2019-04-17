8cda751a9b93cad9203a0efb70611dbeb556606d
olizzz <xinchen.zhao.20@dartmouth.edu>
2019-04-17 17:23:53 -0400

    CSS in JS

--- src/youtube-api.js
+++ src/youtube-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://www.googleapis.com/youtube/v3/search';
-const API_KEY = 'YOUR API KEY'; // add your own key here
+const API_KEY = 'AIzaSyDj11hsZsmIrJzfLfFRPlo3GPhSaFiNlDM'; // add your own key here
 
 const youtubeSearch = (term) => {
   const params = {
